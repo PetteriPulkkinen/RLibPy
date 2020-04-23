a081503d5140799d300013d52e143d76992a7ec6
pulkkip6 <petteri.pulkkinen@aalto.fi>
2020-04-23 15:23:34 +0300

    Add setup
    Signed-off-by: pulkkip6 <petteri.pulkkinen@aalto.fi>

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_namespace_packages
+
+setup(name='RLibPy',
+      version='0.1',
+      description='Reinforcement learning algorithms for OpenAI gym environments.',
+      url='https://github.com/PetteriPulkkinen/RLibPy.git',
+      author='Petteri Pulkkinen',
+      author_email='petteri.pulkkinen@aalto.fi',
+      licence='MIT',
+      packages=find_namespace_packages(),
+      install_requires=[
+            'numpy',
+      ],
+      zip_safe=False)
